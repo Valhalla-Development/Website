ed6c1d46829cbfb344fff844c976b07d86a8225d
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-05-29 15:51:49 +0100

    init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Maintenance } from '../components/Maintenance/Maintenance';
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+
+export default function HomePage() {
+  return (
+    <>
+      <Maintenance />
+      <ColorSchemeToggle />
+    </>
+  );
+}
