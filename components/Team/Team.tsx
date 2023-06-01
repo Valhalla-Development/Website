405e7ab376952207907da1ea8646889a711171c2
mrdennis1212 <dennis.lueke@proton.me>
2023-06-01 18:08:07 +0200

    added module and form to contact person selected

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -1,4 +1,17 @@
-import { Avatar, Text, Button, Paper } from "@mantine/core";
+import {
+  Avatar,
+  Text,
+  Paper,
+  Modal,
+  Button,
+  Group,
+  Title,
+  SimpleGrid,
+  TextInput,
+  Textarea,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
 
 interface UserInfoActionProps {
   avatar: string;
@@ -13,6 +26,21 @@ export function UserInfoAction({
   email,
   job,
 }: UserInfoActionProps) {
+  const [opened, { open, close }] = useDisclosure(false);
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    },
+    validate: {
+      name: (value) => value.trim().length < 2,
+      email: (value) => !/^\S+@\S+$/.test(value),
+      subject: (value) => value.trim().length === 0,
+    },
+  });
+
   return (
     <Paper
       radius="md"
@@ -31,7 +59,70 @@ export function UserInfoAction({
         {email} • {job}
       </Text>
 
-      <Button variant="default" fullWidth mt="md">
+      <Modal opened={opened} onClose={close}>
+        <form onSubmit={form.onSubmit(() => {})}>
+          <Title
+            order={2}
+            size="h1"
+            sx={(theme) => ({
+              fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+            })}
+            weight={900}
+            align="center"
+          >
+            Get in touch
+          </Title>
+
+          <SimpleGrid
+            cols={2}
+            mt="xl"
+            breakpoints={[{ maxWidth: "sm", cols: 1 }]}
+          >
+            <TextInput
+              label="Name"
+              placeholder="Your name"
+              name="name"
+              variant="filled"
+              {...form.getInputProps("name")}
+            />
+            <TextInput
+              label="Email"
+              placeholder="Your email"
+              name="email"
+              variant="filled"
+              {...form.getInputProps("email")}
+            />
+          </SimpleGrid>
+
+          <TextInput
+            label="Subject"
+            placeholder="Subject"
+            mt="md"
+            name="subject"
+            variant="filled"
+            {...form.getInputProps("subject")}
+          />
+          <Textarea
+            mt="md"
+            label="Message"
+            placeholder="Your message"
+            maxRows={10}
+            minRows={5}
+            autosize
+            name="message"
+            variant="filled"
+            {...form.getInputProps("message")}
+          />
+
+          <Group position="center" mt="xl">
+            <Button type="submit" size="md">
+              Send message
+            </Button>
+          </Group>
+        </form>
+      </Modal>
+
+      <Button onClick={open} variant="default" fullWidth mt="md">
         Send message
       </Button>
     </Paper>
