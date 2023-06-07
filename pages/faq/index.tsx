33f5845e0aab341e1559fc1572173e4a634d6e92
iMidnight <mohaidarus@gmail.com>
2023-06-07 19:28:21 +0100

    Added key for FAQ items

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -1,80 +1,67 @@
-import {
-  Image,
-  Accordion,
-  Grid,
-  Col,
-  Container,
-  Title,
-} from "@mantine/core";
+import { Image, Accordion, Grid, Col, Container, Title } from "@mantine/core";
 import useStyles from "./FAQ.styles";
 
-import { GetServerSideProps, InferGetServerSidePropsType } from "next"
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 type Question = {
-  question: string;
-  answer: string;
-  id: string;
-}
+    question: string;
+    answer: string;
+    id: string;
+};
 
 export default function Faq({ questions }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const { classes } = useStyles();
-  return (
-    <div className={classes.wrapper}>
-      <Container size="lg">
-        <Grid id="faq-grid" gutter={50}>
-          <Col span={12} md={6}>
-            <Image src='./faq.svg' alt="Frequently Asked Questions" />
-          </Col>
-          <Col span={12} md={6}>
-            <Title order={2} ta="left" className={classes.title}>
-              Frequently Asked Questions
-            </Title>
+    const { classes } = useStyles();
+    return (
+        <div className={classes.wrapper}>
+            <Container size="lg">
+                <Grid id="faq-grid" gutter={50}>
+                    <Col span={12} md={6}>
+                        <Image src="./faq.svg" alt="Frequently Asked Questions" />
+                    </Col>
+                    <Col span={12} md={6}>
+                        <Title order={2} ta="left" className={classes.title}>
+                            Frequently Asked Questions
+                        </Title>
 
-            <Accordion
-              chevronPosition="right"
-              defaultValue="reset-password"
-              variant="separated"
-            >
-              {
-                questions.map((question, index) => {
-                  return (
-                    <Accordion.Item className={classes.item} value={question.id}>
-                      <Accordion.Control>{question.question}</Accordion.Control>
-                      <Accordion.Panel>{question.answer}</Accordion.Panel>
-                    </Accordion.Item>
-                  )
-                })
-              }
-            </Accordion>
-          </Col>
-        </Grid>
-      </Container>
-    </div>
-  );
+                        <Accordion chevronPosition="right" defaultValue="reset-password" variant="separated">
+                            {questions.map((question, index) => {
+                                return (
+                                    <Accordion.Item className={classes.item} value={question.id} key={index}>
+                                        <Accordion.Control>{question.question}</Accordion.Control>
+                                        <Accordion.Panel>{question.answer}</Accordion.Panel>
+                                    </Accordion.Item>
+                                );
+                            })}
+                        </Accordion>
+                    </Col>
+                </Grid>
+            </Container>
+        </div>
+    );
 }
 
 export const getServerSideProps: GetServerSideProps<{
-  questions: Question[]
+    questions: Question[];
 }> = async (context) => {
-  return {
-      props: {
-          questions: [
-            {
-              question: "Are the bots open source?",
-              answer: "The Seer, Wilbur and the API are open source but we also host them for you so you can use them!",
-              id: "open-source"
-            },
-            {
-              question: "How can I get access to the API?",
-              answer: "If you want to get access to our API please join our Discord Server and open a ticket so we can create a API key for you!",
-              id: "api-access"
-            },
-            {
-              question: "Are costs involved with using the API?",
-              answer: "No, the API is free to usessssss!",
-              id: "api-costs"
-            }
-          ]
-      }
-  }
-}
+    return {
+        props: {
+            questions: [
+                {
+                    question: "Are the bots open source?",
+                    answer: "The Seer, Wilbur and the API are open source but we also host them for you so you can use them!",
+                    id: "open-source"
+                },
+                {
+                    question: "How can I get access to the API?",
+                    answer: "If you want to get access to our API please join our Discord Server and open a ticket so we can create a API key for you!",
+                    id: "api-access"
+                },
+                {
+                    question: "Are costs involved with using the API?",
+                    answer: "No, the API is free to usessssss!",
+                    id: "api-costs"
+                }
+            ]
+        }
+    };
+};
