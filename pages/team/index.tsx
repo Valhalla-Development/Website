f8db7f7b08a4ad1665fa1430c83daa4193e8cac9
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-06-13 20:55:06 +0100

    Updated Ragnar about & moved iMidnight position

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -96,9 +96,10 @@ export const getServerSideProps: GetServerSideProps<{
             {
               name: "Ragnar",
               email: "ragnarlothbrokjr@proton.me",
-              description: "Ragnar is the head developer of Valhalla Development. He is the one goat",
+              description:
+                  "As the founder of Valhalla Development, I believe in the power of community collaboration. I started this journey with a goal of uniting everyone to contribute to our projects. I'm proud to lead a team that shares this vision, as we work together on innovative solutions.",
               pfp: "https://cdn.discordapp.com/avatars/151516555757223936/e4f075a62b5e2719c356ef1be855fa9f.webp?size=2048",
-              position: "Head Developer"
+              position: "Founder | Head Developer"
             },
             {
               name: "zeen",
@@ -108,19 +109,19 @@ export const getServerSideProps: GetServerSideProps<{
               position: "Developer"
             },
             {
+              name: "iMidnight",
+              email: "mohaidarus@gmail.com",
+              description: "Uhh I'm a developer and I like to code stuff i guess idk",
+              pfp: "https://cdn.discordapp.com/avatars/427534456169955352/fa2b09ac444227fc4a68dbbf18bfb3f8.webp?size=2048",
+              position: "Developer",
+            },
+            {
               name: "DanelSonic123",
               email: "danelsonic123@serverargentina.com",
               description: `"no about" - DanelSonic123 when asked for an 'About Me' description.`,
               pfp: "https://cdn.discordapp.com/avatars/1018927667174723615/438a4b6ac24a9ea966bf4ba8d47c6bc0.webp?size=2048",
               position: "System Administrator"
             },
-            {
-              name: "iMidnight",
-              email: "mohaidarus@gmail.com",
-              description: "Uhh I'm a developer and I like to code stuff i guess idk",
-              pfp: "https://cdn.discordapp.com/avatars/427534456169955352/fa2b09ac444227fc4a68dbbf18bfb3f8.webp?size=2048",
-              position: "Rookie Web Dev",
-            }
           ]
       }
   }
