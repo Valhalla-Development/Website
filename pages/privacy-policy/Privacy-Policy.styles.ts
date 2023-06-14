2cbfe93379d9348eee5ade290b706ad5918e7629
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-06-14 18:25:17 +0100

    Moved privacy styles to .styles

---
+++ pages/privacy-policy/Privacy-Policy.styles.ts
@@ -0,0 +1,16 @@
+import { createStyles, rem } from "@mantine/core";
+
+export default createStyles((theme) => ({
+    wrapper: {
+        padding: `calc(${theme.spacing.xl} * 2) ${theme.spacing.xl}`
+    },
+
+    title: {
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        fontSize: rem(36),
+        fontWeight: 900,
+        lineHeight: 1.1,
+        marginBottom: theme.spacing.md,
+        color: theme.colorScheme === "dark" ? theme.white : theme.black
+    }
+}));
--- pages/privacy-policy/index.tsx
+++ pages/privacy-policy/index.tsx
@@ -1,19 +1,6 @@
-import { createStyles, Title, Text, Grid, Col, rem, Container } from "@mantine/core";
+import { Title, Text, Grid, Col, rem, Container } from "@mantine/core";
 
-const useStyles = createStyles((theme) => ({
-    wrapper: {
-        padding: `calc(${theme.spacing.xl} * 2) ${theme.spacing.xl}`
-    },
-
-    title: {
-        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-        fontSize: rem(36),
-        fontWeight: 900,
-        lineHeight: 1.1,
-        marginBottom: theme.spacing.md,
-        color: theme.colorScheme === "dark" ? theme.white : theme.black
-    }
-}));
+import useStyles from "./Privacy-Policy.styles";
 
 export default function privacyPolicy() {
     const { classes } = useStyles();
