8991e7275fbe03f69a73f55ecb2223208c3e6801
iMidnight <mohaidarus@gmail.com>
2023-06-17 14:49:14 +0100

    Implemented blog search

--- pages/api/blog.ts
+++ pages/api/blog.ts
@@ -152,7 +152,7 @@ export default function hander(req: NextApiRequest, res: NextApiResponse) {
                 "Resident Evil Village is a direct sequel to 2017’s Resident Evil 7, but takes a very different direction to its predecessor, namely the fact that this time round instead of fighting against various mutated zombies, you’re now dealing with more occult enemies like werewolves and vampires.",
 
             author: {
-                name: "Ragnar Lotbrok",
+                name: "Ragnar midnuight",
                 image: "https://images.unsplash.com/photo-1593229874334-90d965f27c42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80"
             },
             rating: "outstanding"
@@ -160,7 +160,7 @@ export default function hander(req: NextApiRequest, res: NextApiResponse) {
         {
             image: "https://i.imgur.com/Cij5vdL.png",
             link: "",
-            title: "Resident Evil Village review",
+            title: "midnuight Evil Village review",
             description:
                 "Resident Evil Village is a direct sequel to 2017’s Resident Evil 7, but takes a very different direction to its predecessor, namely the fact that this time round instead of fighting against various mutated zombies, you’re now dealing with more occult enemies like werewolves and vampires.",
 
@@ -212,8 +212,8 @@ export default function hander(req: NextApiRequest, res: NextApiResponse) {
     ];
 
     const info = {
-        items: posts.length,
-        pages: Math.floor(posts.length / 4) + 1
+        items: posts.length
+        // pages:
     };
 
     res.status(200).json({
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -9,17 +9,66 @@ import { useState } from "react";
 
 export default function Faq({ blog }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     const { classes } = useStyles();
+    const [posts, setPosts] = useState(blog.posts);
     const [activePage, setPage] = useState(1);
 
     const chunk = (arr: any[], size: number) => Array.from({ length: Math.ceil(arr.length / size) }, (v, i) => arr.slice(i * size, i * size + size));
-    const arrChunks = chunk(blog.posts, 4);
+    const arrChunks = chunk(posts, 4);
+
     const currentChunk = arrChunks[activePage - 1];
 
     return (
         <div className={classes.wrapper}>
             <Container size="lg">
                 <Container size="sm">
-                    <InputWithButton />
+                    <InputWithButton
+                        placeholder="Search articles"
+                        style={{
+                            justifyContent: "center"
+                        }}
+                        onChange={(event) => {
+                            setPage(1);
+                            if (!event.currentTarget.value) return setPosts(blog.posts);
+
+                            const filtered = blog.posts.filter((post) => {
+                                const searchValue = event.currentTarget.value.toLowerCase();
+                                const titleMatch = post.title.toLowerCase().includes(searchValue);
+                                const descriptionMatch = post.description.toLowerCase().includes(searchValue);
+                                const authorMatch = post.author.name.toLowerCase().includes(searchValue);
+                                const reviewMatch = post.rating.toLowerCase().includes(searchValue);
+
+                                return titleMatch || descriptionMatch || authorMatch || reviewMatch;
+                            });
+
+                            filtered.sort((postA, postB) => {
+                                const searchValue = event.currentTarget.value.toLowerCase();
+                                const titleMatchA = postA.title.toLowerCase().includes(searchValue);
+                                const titleMatchB = postB.title.toLowerCase().includes(searchValue);
+                                const descriptionMatchA = postA.description.toLowerCase().includes(searchValue);
+                                const descriptionMatchB = postB.description.toLowerCase().includes(searchValue);
+                                const authorMatchA = postA.author.name.toLowerCase().includes(searchValue);
+                                const authorMatchB = postB.author.name.toLowerCase().includes(searchValue);
+                                const reviewMatchA = postA.rating.toLowerCase().includes(searchValue);
+                                const reviewMatchB = postB.rating.toLowerCase().includes(searchValue);
+
+                                // Sort by highest match
+                                if (titleMatchA !== titleMatchB) {
+                                    return titleMatchB ? 1 : -1;
+                                } else if (descriptionMatchA !== descriptionMatchB) {
+                                    return descriptionMatchB ? 1 : -1;
+                                } else if (authorMatchA !== authorMatchB) {
+                                    return authorMatchB ? 1 : -1;
+                                } else if (reviewMatchA !== reviewMatchB) {
+                                    return reviewMatchB ? 1 : -1;
+                                }
+
+                                // If all matches are the same, maintain the original order
+                                return 0;
+                            });
+
+                            setPosts(filtered);
+                        }}
+                    />
                 </Container>
                 <Grid
                     mt={40}
@@ -27,17 +76,21 @@ export default function Faq({ blog }: InferGetServerSidePropsType<typeof getServ
                         justifyContent: "center"
                     }}
                 >
-                    {currentChunk.map((post, index) => (
-                        <Grid.Col xs={3} key={index}>
-                            <ArticleCard image={post.image} link={post.link} title={post.title} description={post.description} author={post.author} rating={post.rating} />
-                        </Grid.Col>
-                    ))}
+                    {currentChunk?.length ? (
+                        currentChunk.map((post, index) => (
+                            <Grid.Col xs={3} key={index}>
+                                <ArticleCard image={post.image} link={post.link} title={post.title} description={post.description} author={post.author} rating={post.rating} />
+                            </Grid.Col>
+                        ))
+                    ) : (
+                        <p>No results found</p>
+                    )}
                 </Grid>
                 <Pagination
                     mt={50}
                     value={activePage}
                     onChange={setPage}
-                    total={blog.pages}
+                    total={Math.floor(posts.length / 4) + 1}
                     align="center"
                     style={{
                         justifyContent: "center"
@@ -56,11 +109,11 @@ function InputWithButton(props: TextInputProps) {
             icon={<IconSearch size="1.1rem" stroke={1.5} />}
             radius="xl"
             size="md"
-            rightSection={
-                <ActionIcon size={32} radius="xl" color={theme.primaryColor} variant="filled">
-                    {theme.dir === "ltr" ? <IconArrowRight size="1.1rem" stroke={1.5} /> : <IconArrowLeft size="1.1rem" stroke={1.5} />}
-                </ActionIcon>
-            }
+            // rightSection={
+            //     <ActionIcon size={32} radius="xl" color={theme.primaryColor} variant="filled">
+            //         {theme.dir === "ltr" ? <IconArrowRight size="1.1rem" stroke={1.5} /> : <IconArrowLeft size="1.1rem" stroke={1.5} />}
+            //     </ActionIcon>
+            // }
             placeholder="Search questions"
             rightSectionWidth={42}
             {...props}
@@ -80,8 +133,6 @@ type APIResponse = {
         };
         rating: string;
     }[];
-    items: number;
-    pages: number;
 };
 
 export const getServerSideProps: GetServerSideProps<{
