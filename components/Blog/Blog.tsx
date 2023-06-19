c064949b2f8f676a9d65d809f26e29b2e6fd7572
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-06-19 15:23:19 +0100

    Init blog template!
    is very ugly rn lmao dont look at it

---
+++ components/Blog/Blog.tsx
@@ -0,0 +1,43 @@
+import {
+    Paper, Text, Image, Avatar, Badge, Container,
+} from '@mantine/core';
+import useStyles from './Blog.styles';
+
+type PostProps = {
+    post: {
+        image: string;
+        link: string;
+        title: string;
+        description: string;
+        author: {
+            name: string;
+            image: string;
+        };
+        rating: string;
+    };
+};
+
+export function Blog({ post }: PostProps) {
+    const { classes } = useStyles();
+
+    return (
+        <Container size="lg" className={classes.container}>
+            <Paper shadow="xs">
+                <div className={classes.authorContainer}>
+                    <Avatar className={classes.authorImage} size="md" src={post.author.image} alt={post.author.name} />
+                    <Text>{post.author.name}</Text>
+                    <Badge variant="gradient" gradient={{ from: 'yellow', to: 'red' }} className={classes.rating}>
+                        {post.rating}
+                    </Badge>
+                </div>
+                <div className={classes.heroImageWrapper}>
+                    <Image className={classes.heroImage} src={post.image} alt={post.title} />
+                </div>
+                <Text className={classes.title} size="xl" weight={500}>{post.title}</Text>
+                <Container>
+                    <Text>{post.description}</Text>
+                </Container>
+            </Paper>
+        </Container>
+    );
+}
