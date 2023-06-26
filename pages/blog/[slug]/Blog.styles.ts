47f932d66917e012cf6911d4583729342c5da2cf
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-06-26 17:32:54 +0100

    Update blog styles

--- pages/blog/[slug]/Blog.styles.ts
+++ pages/blog/[slug]/Blog.styles.ts
@@ -21,11 +21,29 @@ export default createStyles((theme) => ({
     paragraph: {
         marginBottom: theme.spacing.md,
         color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        '& a': {
+            textDecoration: 'none',
+            color: theme.colorScheme === 'dark' ? '#4287f5' : '#1a0dab',
+            transition: 'color 0.3s ease-in-out, transform 0.3s ease-in-out',
+            '&:visited': {
+                color: theme.colorScheme === 'dark' ? '#4287f5' : '#1a0dab',
+                textDecoration: 'none',
+            },
+            '&:hover, &:focus': {
+                color: theme.colorScheme === 'dark' ? '#6a42f5' : '#4a0dab',
+                textDecoration: 'none',
+            },
+            '&:active': {
+                color: theme.colorScheme === 'dark' ? '#4287f5' : '#1a0dab',
+                textDecoration: 'none',
+            },
+        },
     },
 
     grid: {
         [theme.fn.largerThan('md')]: {
             borderRight: '1px solid',
+            height: '50vh',
         },
     },
 
