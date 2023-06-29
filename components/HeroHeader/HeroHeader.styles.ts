0d04db0679a4d070635d08241060987462eedb78
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-06-29 16:31:35 +0100

    Create Hero Header

---
+++ components/HeroHeader/HeroHeader.styles.ts
@@ -0,0 +1,71 @@
+import { createStyles, rem } from '@mantine/core';
+
+export default createStyles((theme) => ({
+    hero: {
+        position: 'relative',
+        backgroundImage:
+            'url(https://ragnarokbot.com/assets/img/header-bg.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+
+    container: {
+        height: rem(700),
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-start',
+        paddingBottom: `calc(${theme.spacing.xl} * 6)`,
+        zIndex: 1,
+        position: 'relative',
+
+        [theme.fn.smallerThan('sm')]: {
+            height: rem(500),
+            paddingBottom: `calc(${theme.spacing.xl} * 3)`,
+        },
+    },
+
+    title: {
+        color: theme.white,
+        fontSize: rem(60),
+        fontWeight: 900,
+        lineHeight: 1.1,
+
+        [theme.fn.smallerThan('sm')]: {
+            fontSize: rem(40),
+            lineHeight: 1.2,
+        },
+
+        [theme.fn.smallerThan('xs')]: {
+            fontSize: rem(28),
+            lineHeight: 1.3,
+        },
+    },
+
+    description: {
+        color: theme.white,
+        maxWidth: 600,
+
+        [theme.fn.smallerThan('sm')]: {
+            maxWidth: '100%',
+            fontSize: theme.fontSizes.sm,
+        },
+    },
+
+    control: {
+        marginTop: `calc(${theme.spacing.xl} * 1.5)`,
+
+        [theme.fn.smallerThan('sm')]: {
+            width: '100%',
+        },
+    },
+
+    buttonGroup: {
+        display: 'flex',
+        gap: '10px',
+    },
+
+    icon: {
+        marginRight: '10px',
+    },
+}));
---
+++ components/HeroHeader/HeroHeader.tsx
@@ -0,0 +1,56 @@
+import {
+    Overlay, Container, Title, Button, Text,
+} from '@mantine/core';
+import { IconBrandDiscord } from '@tabler/icons-react';
+import useStyles from './HeroHeader.styles';
+
+export function HeroHeader() {
+    const { classes } = useStyles();
+
+    return (
+        <div className={classes.hero}>
+            <Overlay
+                gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+                opacity={1}
+                zIndex={0}
+            />
+            <Container className={classes.container}>
+                <Title className={classes.title}>Ragnarok</Title>
+                <Text className={classes.description} size="xl" mt="xl">
+                    Ragnarok is a versatile Discord bot, equipped with a wide range of features from Moderation to a Ticket System, and a custom built Economy System.
+                </Text>
+
+                <div className={classes.buttonGroup}>
+                    <Button
+                        variant="gradient"
+                        size="xl"
+                        radius="xl"
+                        className={classes.control}
+                        component="a"
+                        href="#features"
+                    >
+                        View Features
+                    </Button>
+                    <Button
+                        variant="gradient"
+                        size="xl"
+                        radius="xl"
+                        className={classes.control}
+                        onClick={() => window.open('https://discordapp.com/oauth2/authorize?client_id=508756879564865539&scope=bot%20applications.commands&permissions=415306870006', '_blank')}
+                    >
+                        Invite Ragnarok
+                    </Button>
+                    <Button
+                        variant="gradient"
+                        size="xl"
+                        radius="xl"
+                        className={classes.control}
+                        onClick={() => window.open('https://discord.gg/Q3ZhdRJ', '_blank')}
+                    >
+                        <IconBrandDiscord size="1.8rem" stroke={1.2} className={classes.icon} />Support Server
+                    </Button>
+                </div>
+            </Container>
+        </div>
+    );
+}
