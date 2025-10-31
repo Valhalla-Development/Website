87a357fda0f2671f17da0d2f2363a11120bc7475
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-10-31 19:32:06 +0000

    📝 (layout.tsx): add suppressHydrationWarning to body element to prevent React hydration warning.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning>
                 {children}
             </body>
         </html>
